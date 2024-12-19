d2c767e7dbf3b7b742a8c2c17b462e6857ce252d
excpu <ran@5share.site>
2024-12-19 04:38:16 -0500

    Add

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // 自动清理旧的 dist 文件
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 指定模板文件
+      filename: 'index.html', // 输出文件名，默认也是 index.html
+    }),
+  ],
+};
