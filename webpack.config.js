49d03f9e6618a488fc08ce1b9edfb09c0be5b97b
excpu <ran@5share.site>
2024-12-19 04:47:30 -0500

    edited configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true, // 自动清理旧的 dist 文件
-  },
-  mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // 指定模板文件
-      filename: 'index.html', // 输出文件名，默认也是 index.html
-    }),
-  ],
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // 自动清理旧的 dist 文件
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 指定模板文件
+            filename: 'index.html', // 输出文件名，默认也是 index.html
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 };
